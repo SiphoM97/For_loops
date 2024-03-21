b967eb72ec72219777bb8d14f85750eb88b92b88
Sipho Mzayiya <realsiphomzayiya@gmail.com>
2024-03-21 03:26:09 +0200

    updated/ git check.

--- For_loops.py
+++ For_loops.py
@@ -1,4 +1,46 @@
+# Example one supplement list
 supplements = ["usn", "nutritech", "hellfire", ]
 
 for supplement in supplements:
     print(f"i prefer {supplement}")
+
+# example two company list
+
+silicon_valley = ["Apple", "Tesla", "AirBnb", "Uber", "facebook"]
+
+for company in silicon_valley:
+    print("I would love to work at " + company)
+
+# example 3
+# Prompt the user to enter a positive integer
+num = int(input("Enter a positive integer: "))
+
+#Initialize the factorial to 1
+factorial = 1
+
+# Use a for loop to calculate the factorial
+# Hint: Multiply the factorial by each number from 1 to num
+for i in range(1, num + 1):
+    factorial *= i
+
+#Print the factorial
+print("The factorial of", num, "is", factorial)
+
+# example 4 : using range
+
+
+unicorns = "Hoja is a unicorn company!"
+for i in range(3):
+    print(str(i))
+
+# example 4
+
+shoes = ["air max", "air force1", "jordan 3", "jordan 1", "yeezy desert boots", "fear of gods1"]
+shoe_i_want = "yeezy desert boots"
+
+for shoe in shoes:
+    shoe == shoe_i_want
+    print("placing the order for desired shoe")
+    break
+    print(shoes)
+
